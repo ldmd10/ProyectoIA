384ee36c240a89288950f2e0788ec491378d01e2
luisdavidmd102 <luisdavidmd102@gmail.com>
2019-08-07 16:23:39 -0500

    commit de archivos del modelo algoritmos

---
+++ ProyectoIA/Apps/algoritmo/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Algoritmo(models.Model):
+    nombreAlgoritmo=models.CharField(max_length=200, null=True, blank=True)
+    tipo=models.CharField(max_length=100, null=True,blank=True)
+    
+
+    def __str__(self):
+        return '{}'.format(self.nombreAlgoritmo)
+
+    def ejecutarAlgoritmo(self):
+        print("Corriendo algoritmo...")
+
+    def entrenarAlgoritmo(self):
+        print("Entrenando algoritmo..")
+
+
+class Entrenamiento(models.Model):
+    idEntrenamiento=models.AutoField(primary_key=True, max_length=10)
+    tituloEntrenamiento=models.CharField(max_length=80)
+    foraneaAlgoritmo=models.ForeignKey(Algoritmo,null=False,on_delete=models.CASCADE)
+    rutaArchivo = models.FileField(upload_to='Archivos/', null=True, blank=True)  # organizar upload to

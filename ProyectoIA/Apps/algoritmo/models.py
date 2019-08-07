2e5e8099afd917b4e3010e645ea109868516b6a6
luisdavidmd102 <luisdavidmd102@gmail.com>
2019-08-07 16:52:19 -0500

    okgekwokgew

--- ProyectoIA/Apps/algoritmo/models.py
+++ ProyectoIA/Apps/algoritmo/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
+# oeoeoe gonoreea
+
 # Create your models here.
 class Algoritmo(models.Model):
-    nombreAlgoritmo=models.CharField(max_length=200, null=True, blank=True)
-    tipo=models.CharField(max_length=100, null=True,blank=True)
-    
+    nombreAlgoritmo = models.CharField(max_length=200, null=True, blank=True)
+    tipo = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return '{}'.format(self.nombreAlgoritmo)
@@ -17,7 +19,7 @@ class Algoritmo(models.Model):
 
 
 class Entrenamiento(models.Model):
-    idEntrenamiento=models.AutoField(primary_key=True, max_length=10)
-    tituloEntrenamiento=models.CharField(max_length=80)
-    foraneaAlgoritmo=models.ForeignKey(Algoritmo,null=False,on_delete=models.CASCADE)
+    idEntrenamiento = models.AutoField(primary_key=True, max_length=10)
+    tituloEntrenamiento = models.CharField(max_length=80)
+    foraneaAlgoritmo = models.ForeignKey(Algoritmo, null=False, on_delete=models.CASCADE)
     rutaArchivo = models.FileField(upload_to='Archivos/', null=True, blank=True)  # organizar upload to
